904a2ca08f21d1cf1d57ce7609279f26381434cc
Matetorture <przemek.pytlarz18@gmail.com>
2024-05-06 17:01:38 +0200

    init project

---
+++ recvoicer/recvoicer/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace recvoicer
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ recvoicer/recvoicer/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace recvoicer
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
